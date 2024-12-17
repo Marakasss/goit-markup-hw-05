e518cac1ffcd53b76d4016c8222fa3d2ca1169c1
Marakas <igorpetriv.bsc@gmail.com>
2024-12-17 19:38:15 +0200

    add file script.js/remove script from html

---
+++ script.js
@@ -0,0 +1,16 @@
+ const modal_openBtn = document.getElementById('modal_openBtn');
+      const modal_closeBtn = document.getElementById('modal_closeBtn');
+      const modalOverlay = document.getElementById('modalOverlay');
+
+      const openModal = () => {
+        modalOverlay.classList.add('is-open');
+      };
+
+      const closeModal = () => {
+        modalOverlay.classList.remove('is-open');
+      };
+
+modal_openBtn.addEventListener('click', openModal);
+modal_closeBtn.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', closeModal);
+      
